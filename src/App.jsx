d050b2d026272db3cc427aaad951542bd1a74cab
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-10 17:37:50 +0200

    feat: add useEffect to filter movies genre

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
+import { useState, useEffect } from 'react';
 import movies from './data/movies.js';
+
+
 
 function App() {
 
+  // useState che corrisponde all'array di oggetti
+  const [moviesList, setMoviesList] = useState(movies);
+  // useState iniziale del campo select --> inizialmente è vuoto
+  const [searchGenre, setSearchGenre] = useState('');
+
+  // useEffect per filtrare i film per genere
+  useEffect(() => {
+
+    let result = movies;
+
+    if (searchGenre !== '') {
+
+      result = movies.filter(movie => movie.genre === searchGenre)
+    }
+
+    setMoviesList(result);
+
+  }, [searchGenre])
+
   return (
     <>
-      <div>
-        <h1>Movie Blog</h1>
-      </div>
-      {movies.map((movie, i) =>
+      <h1>Movie Blog</h1>
+
+      <section>
+        <label>Cerca per genere</label>
+        <div>
+          <select value={searchGenre} onChange={event => setSearchGenre(event.target.value)}>
+            <option>---</option>
+            <option>Fantascienza</option>
+            <option>Thriller</option>
+            <option>Romantico</option>
+            <option>Azione</option>
+          </select>
+        </div>
+      </section>
+
+      {moviesList.map((movie, i) =>
         <section key={i}>
           <h3>{movie.title}</h3>
           <p>{movie.genre}</p>
